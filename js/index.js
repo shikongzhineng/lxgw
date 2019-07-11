0603aa7344dfc6b7d489c4b596dbcf248039a846
skzn <shikongzhineng@163.com>
2019-07-11 21:12:30 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// $(function(){
+// $('.rmzy .col').each((i, elem) => {
+//   // console.log(elem);
+//   elem.onmouseleave=elem.onmouseenter = function (e) {
+//     // console.log(e.offsetX);
+//     // console.log(e.offsetY);
+//     // console.log(this);
+//     var elBack=this.lastElementChild;
+//     var $elBack=$(elBack);
+//     console.log(elBack.className,i);
+//     var t=setTimeout(()=>{
+//       $elBack.toggleClass('hover');
+//     console.log(elBack.className);
+//     clearTimeout(t);
+//     },100);
+//   }
+// })
+// })
